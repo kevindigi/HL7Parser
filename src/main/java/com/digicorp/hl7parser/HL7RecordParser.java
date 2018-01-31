021c233f5c11ce7c25f029a92ee27a5795bedc27
Kevin B. Adesara <kevin.adesara@digi-corp.com>
2018-01-31 15:01:25 +0530

    HL7 Record Parser

---
+++ src/main/java/com/digicorp/hl7parser/HL7RecordParser.java
@@ -0,0 +1,126 @@
+/*
+
+MSH|^~\&|ENGINE|013|HMS|013|20171102200005||ORU^R01|20171103000136|P|2.3
+PID|||497974^^^ORSC03||SMITH^KALANA^N||19890426|F||B|425 ALMA BYRD LN APT A^^SPARTANBURG^SC^29301|083|(864)310-8055||E|M|B|3908060|251772775
+
+*/
+
+
+package com.digicorp.hl7parser;
+
+import java.util.LinkedHashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * @author kevin.adesara on 31/01/18.
+ */
+public class HL7RecordParser {
+    private LinkedHashMap<String, String> data = new LinkedHashMap<>();
+    private String subFieldSeparators = "";
+
+    public HL7RecordParser() {
+    }
+
+    private String getSubFieldSeparators() {
+        return subFieldSeparators;
+    }
+
+    /**
+     * Set global sub field separators
+     *
+     * @param sep sub field separator like ^~\&
+     */
+    void setSubFieldSeparators(String sep) {
+        subFieldSeparators = sep;
+    }
+
+    /**
+     * Check whether sep is sub field separator or not
+     *
+     * @param sep separator character
+     * @return true if sep is sub field separator, false otherwise
+     */
+    private boolean isSubFieldSeparator(String sep) {
+        return subFieldSeparators.contains(sep);
+    }
+
+    void parseRecord(String[] record) {
+        String key = record[0];
+        for (int i = 0; i < record.length; i++) {
+            String fieldKey = key + '.' + i;
+            parseField(fieldKey, record[i]);
+        }
+    }
+
+    private void parseField(String fieldKey, String field) {
+        if (data.containsKey(fieldKey)) {
+            String existingField = data.get(fieldKey);
+            if (existingField != null && !existingField.trim().equalsIgnoreCase(field.trim())) {
+                data.put(fieldKey, existingField + "\n" + field);
+            }
+        } else {
+            data.put(fieldKey, field);
+        }
+
+        if (!hasSubFields(field) || field.equalsIgnoreCase(getSubFieldSeparators())) {
+            return;
+        }
+
+        String[] subFields = getSubFields(field);
+        if (subFields.length == 1) {
+            data.put(fieldKey, field);
+            return;
+        }
+
+        for (int i = 0; i < subFields.length; i++) {
+            parseField(fieldKey + "." + (i+1), subFields[i]);
+        }
+    }
+
+    private boolean hasSubFields(String field) {
+        for (int i = 0; i < field.length(); i++) {
+            String c = Character.toString(field.charAt(i));
+            if (isSubFieldSeparator(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private String getSubFieldSeparator(String field) {
+        for (int i = 0; i < field.length(); i++) {
+            String c = Character.toString(field.charAt(i));
+            if (isSubFieldSeparator(c)) {
+                return c;
+            }
+        }
+        return null;
+    }
+
+    private String[] getSubFields(String field) {
+        String subFieldSeparator = getSubFieldSeparator(field);
+        if (subFieldSeparator == null) {
+            return new String[]{field};
+        }
+
+        subFieldSeparator = escapeMetaCharacters(subFieldSeparator);
+        return field.split(subFieldSeparator);
+    }
+
+    private String escapeMetaCharacters(String inputString) {
+        final String[] metaCharacters = {"\\", "^", "$", "{", "}", "[", "]", "(", ")", ".", "*", "+", "?", "|", "<", ">", "-", "&"};
+        String outputString = "";
+        for (String metaCharacter : metaCharacters) {
+            if (inputString.contains(metaCharacter)) {
+                outputString = inputString.replace(metaCharacter, "\\" + metaCharacter);
+                inputString = outputString;
+            }
+        }
+        return outputString;
+    }
+
+    void log() {
+        Logger.getGlobal().log(Level.INFO, data.toString());
+    }
+}
--- src/main/java/com/digicorp/hl7parser/Main.java
+++ src/main/java/com/digicorp/hl7parser/Main.java
@@ -1,5 +1,27 @@
+package com.digicorp.hl7parser;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //MSH|^~\&|ENGINE|013|HMS|013|20171102200005||ORU^R01|20171103000136|P|2.3
+        //PID|||497974^^^ORSC03||SMITH^KALANA^N||19890426|F||B|425 ALMA BYRD LN APT A^^SPARTANBURG^SC^29301|083|(864)310-8055||E|M|B|3908060|251772775
+        String[][] records = new String[][]{
+                {"MSH", "^~\\&", "ENGINE", "013", "HMS", "013", "20171102200005", "ORU^R01", "20171103000136", "P", "2.3"},
+                {"PID", "", "", "497974^^^ORSC03", "", "SMITH^KALANA^N", "", "19890426", "F", "", "B", "425 ALMA BYRD LN APT A^^SPARTANBURG^SC^29301", "083", "(864)310-8055", "", "E", "M", "B", "3908060", "251772775"},
+                {"PV1", "", "", "EOP^^ORSC03", "", "", "", "10446^FORGIONE^LISA^M", "10446^FORGIONE^LISA^M", "", "", "", "", "", "", "", "", "10446^FORGIONE^LISA^M", "", "3908060", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""},
+                {"ORC", "RE", "39080600000100", "39080600000100", "", "", "", "1^^^20171102200005^^S^^STAT", "", "20171102235425", "", "", "80135^KENT^JILLIAN^R", "MARY BLACK MEMORIAL HOSPITAL", "", "", "", "MARY BLACK MEMORIAL HOSPITAL"},
+                {"OBR", "1", "39080600000100", "39080600000100", "IM^CT HEAD WO CONTRAST^CT^IM", "", "20171102200005", "20171102200005", "20171102200005", "", "", "", "", "", "20171102172700", "", "no referring doctor^REFDOC^EMPTY", "", "", "", "", "", "20171102200005", "", "CT", "F", "", "1^^^20171102172700^^S^^STAT", "", "", "", "^Sitting there and got real hot and dizzy, started vomiting;  c/o ha, N/V;  acute onset", "10276^NATTELL MD^DANIEL", "", "", "", "20171102235425", "", "", "", "", "", "", "", "CTHD^CT HEAD WO CONTRAST"},
+                {"OBX", "1", "TX", "CTHD^CT HEAD WO CONTRAST^CT", "", "                       Mary Black Health System", "                       ", "                       ", "                       ", "                       ", "                       ", "                       F", "                       ", "                       ", "                       20171102200005", "                       ", "                       10276^NATTELL MD^DANIEL", "                       ", "                       FINAL"},
+                {"OBX", "2", "TX", "CTHD^CT HEAD WO CONTRAST^CT", "", "                          1700 Skylyn Drive", "                          ", "                          ", "                          ", "                          ", "                          ", "                          F", "                          ", "                          ", "                          20171102200005", "                          ", "                          10276^NATTELL MD^DANIEL", "                          ", "                          FINAL"}
+        };
+
+        HL7RecordParser parser = new HL7RecordParser();
+        for (String record[] : records) {
+            if (record[0].equalsIgnoreCase("msh")) {
+                parser.setSubFieldSeparators(record[1]);
+            }
+            parser.parseRecord(record);
+        }
+
+        parser.log();
     }
 }
